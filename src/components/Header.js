8176cbb5cdd328efbc3355c5c07b197dcdf6fa2d
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-02-24 23:37:40 -0500

    updated app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import axios, { Axios } from 'axios';
+import styled from 'styled-components';
 
 function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
+	const getJoke = () => {
+		Axios.get("https://official-joke-api.appspot.com/random_joke").then(
+			(response) => {
+				console.log(response);
+			}
+		)
+	 }
+ return (
+	<ContainerHead>
+		<h2> Best Joke Evea! </h2>
+		<input placeholder='hold on tight!'></input>
+		<button onClick={getJoke}> Laugh Button </button>
+	</ContainerHead>
+ )
 }
 
 export default Header;
+
+const ContainerHead = styled. div`
+	
+`;
+
